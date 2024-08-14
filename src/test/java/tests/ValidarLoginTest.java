a632d91b90e0af078bac4fba1d514a1e703a5ede
Felipe Barra <felipevitalino96@gmail.com>
2024-08-14 18:30:11 -0300

    Update ValidarLogin

--- src/test/java/tests/ValidarLoginTest.java
+++ src/test/java/tests/ValidarLoginTest.java
@@ -2,8 +2,12 @@ package tests;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class ValidarLoginTest {
     @Test
@@ -13,17 +17,44 @@ public class ValidarLoginTest {
         WebDriver navegador = new ChromeDriver();
 
         // Navegando para a página "saucedemo"
-            navegador.get("https://www.saucedemo.com/");
+        navegador.get("https://www.saucedemo.com/");
+
+        /*
+        *
+        * Identificadores dos campos:
+        *
+        * [TXT] Username - id = user-name
+        * [TXT] Password - id = password
+        * [BTN] Login - id = login-button
+        *
+        */
+
+        WebElement txt_username = navegador.findElement(By.id("user-name"));
+        WebElement txt_password = navegador.findElement(By.id("password"));
+        WebElement btn_login = navegador.findElement(By.id("login-button"));
 
         // Cenário 1: Teste de login bem-sucedido
+        txt_username.sendKeys("standard_user");
+        txt_password.sendKeys("secret_sauce");
+        btn_login.click();
+        navegador.quit();
 
         // Cenário 2: Teste de login com nome de usuário incorreto
+        txt_username.sendKeys("felipe");
+        txt_password.sendKeys("secret_sauce");
+        btn_login.click();
+        navegador.quit();
 
         // Cenário 3: Teste de login com senha incorreta
+        txt_username.sendKeys("standard_user");
+        txt_password.sendKeys("123456");
+        btn_login.click();
+        navegador.quit();
 
         // Cenário 4: Teste de campos vazios
-
-        // Validação
-        assertEquals(1,1);
+        txt_username.sendKeys();
+        txt_password.sendKeys();
+        btn_login.click();
+        navegador.quit();
     }
 }
