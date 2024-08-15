7e62284013e8821badef377fc25420093870e571
Felipe Barra <felipevitalino96@gmail.com>
2024-08-15 13:31:36 -0300

    Update 08/15
    Incluído as anotações @After, @Before e criado métodos para cada cenário de teste.

--- src/test/java/tests/ValidarLoginTest.java
+++ src/test/java/tests/ValidarLoginTest.java
@@ -1,6 +1,10 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,51 +14,90 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class ValidarLoginTest {
-    @Test
-    public void testValidarLogin(){
+    private WebDriver navegador;
+    private WebElement txt_username;
+    private WebElement txt_password;
+    private WebElement btn_login;
+
+    @Before
+    public void setUp(){
         // Abrindo o navegador
         System.setProperty("webdriver.chrome.driver","C:\\Users\\felip\\drivers\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
+        navegador = new ChromeDriver();
 
         // Navegando para a página "saucedemo"
         navegador.get("https://www.saucedemo.com/");
 
-        /*
-        *
-        * Identificadores dos campos:
-        *
-        * [TXT] Username - id = user-name
-        * [TXT] Password - id = password
-        * [BTN] Login - id = login-button
-        *
-        */
+        txt_username = navegador.findElement(By.id("user-name"));
+        txt_password = navegador.findElement(By.id("password"));
+        btn_login = navegador.findElement(By.id("login-button"));
+    }
 
-        WebElement txt_username = navegador.findElement(By.id("user-name"));
-        WebElement txt_password = navegador.findElement(By.id("password"));
-        WebElement btn_login = navegador.findElement(By.id("login-button"));
+    /*
+     *
+     * Identificadores dos campos:
+     *
+     * [TXT] Username - id = user-name
+     * [TXT] Password - id = password
+     * [BTN] Login - id = login-button
+     *
+     */
 
+    @Test
+    public void testValidarLoginComSucesso() {
         // Cenário 1: Teste de login bem-sucedido
         txt_username.sendKeys("standard_user");
         txt_password.sendKeys("secret_sauce");
         btn_login.click();
-        navegador.quit();
+    }
 
+    @Test
+    public void testValidarLoginComUsuarioIncorreto() {
         // Cenário 2: Teste de login com nome de usuário incorreto
         txt_username.sendKeys("felipe");
         txt_password.sendKeys("secret_sauce");
         btn_login.click();
-        navegador.quit();
+    }
 
+    @Test
+    public void testValidarLoginComSenhaIncorreta() {
         // Cenário 3: Teste de login com senha incorreta
         txt_username.sendKeys("standard_user");
         txt_password.sendKeys("123456");
         btn_login.click();
-        navegador.quit();
+    }
 
+    @Test
+    public void testValidarCamposVazios(){
         // Cenário 4: Teste de campos vazios
-        txt_username.sendKeys();
-        txt_password.sendKeys();
         btn_login.click();
+    }
+
+    @Test
+    public void testValidarCamposPasswordVazio(){
+        // Cenário 5: Tentativa de login apenas digitando o username
+        txt_username.sendKeys("standard_user");
+        btn_login.click();
+
+        WebElement senhaEmBranco = navegador.findElement(By.className("error-message-container"));
+        String textoSenhaEmBranco = senhaEmBranco.getText();
+        assertEquals("Epic sadface: Password is required",textoSenhaEmBranco);
+    }
+
+    @Test
+    public void testValidarCampoUsernameVazio(){
+        // Cenário 6: Tentativa de login apenas digitando a senha
+        txt_password.sendKeys("123456");
+        btn_login.click();
+
+       WebElement usernameEmBranco = navegador.findElement(By.className("error-message-container"));
+       String textoUsernameEmBranco = usernameEmBranco.getText();
+       assertEquals("Epic sadface: Username is required",textoUsernameEmBranco);
+    }
+
+    @After
+    public void tearDown(){
+        // Fechar o navegador
         navegador.quit();
     }
 }
